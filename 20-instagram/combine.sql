8b257e127cdbfbcac70aa175f8ae4ac4e348a1e9
Mehedi Hasan <hellomehediwd@gmail.com>
2023-08-21 11:52:14 +0600

    Answer to the question no 01 of 'users', 'caption_tags', 'photo_tags'

---
+++ 20-instagram/combine.sql
@@ -0,0 +1,12 @@
+-- Answer to the questions: starts --
+SELECT users.username, tags.created_at
+FROM users
+JOIN (
+	SELECT user_id, created_at FROM caption_tags
+	UNION ALL
+	SELECT user_id, created_at FROM photo_tags
+) AS tags ON tags.user_id = users.id
+WHERE tags.created_at < '2010-01-07';
+
+
+-- Answer to the questions: ends --
