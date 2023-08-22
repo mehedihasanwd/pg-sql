211f2e0bff71daa8bcce0b58c11fd31d7000b213
Mehedi Hasan <hellomehediwd@gmail.com>
2023-08-22 18:10:05 +0600

    Answer to the question no 10 for the 'combine' questions

--- 20-instagram/combine.sql
+++ 20-instagram/combine.sql
@@ -87,4 +87,16 @@ SELECT username
 FROM recent_posts
 JOIN users ON users.id = recent_posts.user_id;
 
+-- Answer to the question no -> 10
+CREATE OR REPLACE VIEW recent_posts AS (
+	SELECT * 
+	FROM posts
+	ORDER BY created_at DESC
+	LIMIT 15
+);
+
+SELECT * FROM recent_posts;
+
+DROP VIEW recent_posts;
+
 -- Answer to the questions: ends --
