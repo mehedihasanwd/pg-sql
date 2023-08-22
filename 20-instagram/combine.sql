380b2add654e8aa9d90bec9ad801efdd26fb168e
Mehedi Hasan <hellomehediwd@gmail.com>
2023-08-22 16:58:22 +0600

    Answer to the question no 06 for the 'combine' questions

--- 20-instagram/combine.sql
+++ 20-instagram/combine.sql
@@ -52,4 +52,10 @@ JOIN (
 GROUP BY users.username
 ORDER BY COUNT(*) DESC LIMIT 1
 
+CREATE VIEW tags AS (
+	SELECT id, created_at, user_id, post_id, 'photo_tag' AS type FROM photo_tags
+ UNION ALL
+	SELECT id, created_at, user_id, post_id, 'caption_tag' AS type FROM caption_tags
+)
+
 -- Answer to the questions: ends --
