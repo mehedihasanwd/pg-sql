06b3137f7aecfcad89a1bb0df98a17bef635906f
Mehedi Hasan <hellomehediwd@gmail.com>
2023-08-22 14:21:42 +0600

    Answer to the question no 04 for the 'combine' questions

--- 20-instagram/combine.sql
+++ 20-instagram/combine.sql
@@ -26,4 +26,20 @@ WITH RECURSIVE countdown(val) AS (
 SELECT *
 FROM countdown;
 
+WITH RECURSIVE suggestions(leader_id, follower_id, depth) AS (
+		SELECT leader_id, follower_id, 1 AS depth
+		FROM followers
+		WHERE follower_id = 1000
+	UNION
+		SELECT followers.leader_id, followers.follower_id, depth + 1
+		FROM followers 
+		JOIN suggestions ON suggestions.leader_id = followers.follower_id
+		WHERE depth < 3
+)
+SELECT DISTINCT users.id, users.username
+FROM suggestions
+JOIN users ON users.id = suggestions.leader_id
+WHERE depth > 1
+LIMIT 30;
+
 -- Answer to the questions: ends --
